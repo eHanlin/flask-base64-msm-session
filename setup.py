b9144259ed4962603a9f6e9c4e932fd664b0bea4
Sparrow.jang <sparrow.jang@gmail.com>
2016-07-29 17:45:52 +0800

    modified version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except:
     from distutils.core import setup, find_packages
  
 setup(name='flask_base64_msm_session',
-      version='1.0',
+      version='1.0.0',
       description='Use base64 encoder on memcached server. And it will use memcached on session',
       author='Sparrow Jang',
       author_email='sparrow.jang@gmail.com',
